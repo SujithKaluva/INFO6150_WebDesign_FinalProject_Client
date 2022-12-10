b460123039cffedc6d58d41069cd65af3f90b037
Vamsikrishna-7566 <divvela.n@northeastern.edu>
2022-12-09 23:23:49 -0500

    full UI

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,123 +1,62 @@
+
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import './Header.scss'
+import AppLogo from "../../Assets/Images/fa-logo.svg"
+import { Button, IconButton, Typography } from "@mui/material";
+import { logout } from "../../Store/Actions/LoginAction";
 import { connect } from "react-redux";
-import { useState} from 'react';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import { getFoodData, updateHealth } from '../../Store/Actions/JournalAction.js';
-import { useEffect} from 'react';
-import {Typography, Button} from '@mui/material';
+import Notifications from "@mui/icons-material/Notifications";
+import { useState } from "react";
+import NotificationDialog from "../../Containers/NotificationDialog/NotificationDialog";
+import EventNotificationContent from "../../Containers/NotificationDialog/EventNotificationContent";
 
+const Header = (props) => {
+  const navigate = useNavigate();
+  const [openNotif, setOpenNotif] = useState(false);
 
-const FoodSearchInput = (props) => {
-    const [selectedFoodList, setSelectedFoodList] = useState([])
-    const [totalCal, setTotalCal] =  useState(0)
-    const [tempHealthState, setTempHealthState] = useState({...props.customModalProps})
-
-    const { getFoodDataFromAPI, foodNutritionInfo} = props;
-
-    useEffect(() => {
-      props.getFoodDataFromAPI();
-    },[]); 
-
-    //onClick taking the data and add the object into the record and making the api call
-    const onClickSubmit = () => {
-      let tempHealth = {}
-      let historyArray = [...tempHealthState.history];
-      let tempHistory={
-          dateTime: new Date(),
-          totalValue: Number(totalCal)
-      }
-      tempHealth.totalValue=tempHealthState.totalValue+ Number(totalCal)
-      historyArray.push(tempHistory)
-      tempHealth.history = historyArray;
-      tempHealth.userUUID = tempHealthState.userUUID;
-      props.updateHealth(tempHealthState.userUUID, tempHealthState.userActivityId, tempHealth);
-      props.modalClickHandler()
+  const signoutClick =async()=>{
+    let res = await props.logout();
+    if(res){
+      navigate("/login");
+    }
   }
 
-    const onInputChange = (totalCal) => {
-        getFoodDataFromAPI(totalCal);
-    }
+  const onClickOfNotififcation=()=>{
+    setOpenNotif(true);
+  }
 
-    const handleChange = (event, value) => {
-        if(!value || value === "undefined") {
-          return
-        }
-        const totCal = totalCal + parseInt(value?.nutrition?.nutrients[0].amount)
-        setTotalCal(totCal)
-        const newFoodList = [value, ...selectedFoodList]
-        setSelectedFoodList(newFoodList)
-      }
-
-    //funtion to show the suggested food
-    const __renderAutoCompleteBox = () => {
-        const suggestList = foodNutritionInfo?.results
-
-        return (
-          <Autocomplete
-            disablePortal
-            className="food-search"
-            id="food auto complete"
-            options={suggestList}
-            sx={{ width: 600 }}
-            renderInput={(params) => 
-            <TextField        
-              onChange={(e) => onInputChange(e.target.value)}
-              {...params} 
-              label="Search Food" 
-            />}
-            getOptionLabel={(suggestList) => suggestList.title}
-            onChange={handleChange} 
-          />
-        );
-      }
-    
-      //function to render the selected food item and add the calories
-      const __renderSelectedFoodItems = () => {
-        return selectedFoodList && selectedFoodList.map((foodItem, index) => {
-          const calAmount = foodItem?.nutrition?.nutrients[0].amount
-          return foodItem && (
-              <div key={index} className='food-item'>
-                <div className='wrap'>
-                  <img className='food-img' src={foodItem.image} alt=""/>
-                  <span className='food-name'>{foodItem.title}</span>
-                </div>
-                <div className='food-cal'>{calAmount} {foodItem?.nutrition?.nutrients[0]?.unit}</div>
-              </div>
-          )
-        })
-      }
+  const handleNotificationClose =()=>{
+    setOpenNotif(false);
+  }
 
   return (
-    <>
-        <div className='event-search-wrapper'>
-            {__renderAutoCompleteBox()}
-          </div>  
-          <div className='food-items-wrapper'>
-            {__renderSelectedFoodItems()}
-            <p className="total-calories">Total Calories: {totalCal} kcal</p>
+    <div className="main-layout-container"> 
+        <div className="header-wrapper">
+          <div className="header-logo">
+              <img alt="app-logo" src={AppLogo} />
           </div>
-          <div className="save-button-div">
-            <Button onClick={onClickSubmit} className="sub-btn" variant="outlined" autoFocus>
-                <Typography>Save</Typography>
-            </Button>
-          </div>          
-    </>
+            <nav className="header-nav"> 
+                <Typography paddingRight={2} color='secondary.light'><Link to="/">Dashboard</Link></Typography>
+                <Typography paddingRight={2} color='secondary.light'><Link to="/journal">Journal</Link></Typography>
+                <Typography paddingRight={2} color='secondary.light'><Link to="/events">Events</Link></Typography>
+                <Typography paddingRight={2} color='secondary.light'><Link to="/weather">Weather</Link></Typography>
+                <Typography color='secondary.light'><Link to="/profile">Profile</Link></Typography>
+                
+                <IconButton size={"large"} children ={<Notifications className="notifications-btn"/>} onClick={onClickOfNotififcation}/>
+                <Typography paddingRight={2} color='secondary.light'><Button  onClick={signoutClick} variant="outlined" size="small" className="signout-btn">Sign Out</Button></Typography>
+            </nav>
+        </div>
+        <NotificationDialog open={openNotif} handleClose={handleNotificationClose} content={<EventNotificationContent/>}/>
+        <Outlet/>
+    </div> 
   )
   
 }
 
-const mapDisptchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    getFoodDataFromAPI : (inputFoodTxt) => dispatch(getFoodData(inputFoodTxt)),
-    // saveHealthData: (inputFoodTxt) => dispatch(saveHealthDetails(inputFoodTxt)),
-    updateHealth: (uuid, userActivityId, newHealth)=>dispatch(updateHealth(uuid, userActivityId, newHealth))
-  }
-} 
-
-const mapStateToProps = (state) => {
-  return {
-    foodNutritionInfo : state?.Journal?.externalFoodData,
+      logout : () => dispatch(logout()),
   }
 }
 
-export default connect(mapStateToProps, mapDisptchToProps)(FoodSearchInput);
+export default connect(null, mapDispatchToProps)(Header);
